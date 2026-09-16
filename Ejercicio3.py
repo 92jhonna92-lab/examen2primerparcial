ef624db87359ddfdfc7611eb584f8fca5b169546
92jhonna92 <92jhonna92@gmail.com>
2026-09-16 08:43:04 -0400

    thrid

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -3,13 +3,12 @@ if n<1 or n>10:
     print("Fuera de alcance")
 else:
     for a in range(1,n+1):
-        suma=0
+        sumaX=0
         sumaY=0
+        sumatotal=0
         for b in range(1,13):
             c=a*b
        
             print(f"{a} x {b}={c}")
-            sumaX=suma+c
-        print(f"La suma total es: {sumaX}")
-    sumaY=sumaY+sumaX
-    print(f"La suma total es: {sumaY}")
+            sumatotal+=c
+    print(f"La suma total es: {sumatotal}")
